42491afb39b672ef6c540dac37fa0fe59a1cb0e8
anamzvz <anamarvaz2002@gmail.com>
2024-09-29 21:11:06 +0200

    29/09/2024

---
+++ src/main/java/org/example/AMVt01e01.java
@@ -0,0 +1,11 @@
+package org.example;
+
+
+import java.util.Scanner;
+
+public class AMVt01e01 {
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+
+    }
+}
